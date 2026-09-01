406a63f0049d60d4e3fc791ec5e56b09a1c4602e
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-09-02 08:01:13 +0900

    feat: add home page

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.http.response import JsonResponse
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({"status": "success", "message": "Home page"})),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
 ]
